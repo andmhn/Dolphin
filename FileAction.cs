e0a8ee30189f61f3284cdaf724f690da83340ed4
Anand Mohan <ianandmohan@vivaldi.net>
2022-10-26 09:33:12 +0530

    Initial Commit

---
+++ FileAction.cs
@@ -0,0 +1,71 @@
+namespace Dolphin;
+
+class FileAction
+{
+	private string _fileUri;
+	private string _content = "";
+	
+	public FileAction(string fileUri)
+	{
+		_fileUri = fileUri;
+		_content = File.ReadAllText(_fileUri);
+	}
+	
+	public void AllAction()
+	{
+		CountAlphaN();
+		CountChars();
+		CountWords();
+		CountLines();	
+	}
+	
+	public void CountAlphaN()
+	{
+		int counter = 0;
+		foreach (char c in _content){
+			if ((c >= 'A' && c <= 'Z') ||
+				(c >= 'a' && c <= 'z') ||
+				(c >= '0' && c <= '9'))
+			{
+    			counter++;
+            }
+		}
+		Console.WriteLine($"Total Letters in file {_fileUri} : {counter}");
+	}
+	
+	public void CountChars()
+	{
+		int number = _content.Length;
+		Console.WriteLine($"Total Characters in file {_fileUri} : {number}");
+	}
+	
+	public void CountWords()
+	{
+		string[] split_text = _content.Split(' ');
+		string new_text = "";
+
+		foreach(string av in split_text)
+		{
+			if (av != "")
+			{ 
+				new_text = new_text  + av + ",";
+			}
+		}
+		new_text = new_text.TrimEnd(',');
+		split_text = new_text.Split(',');
+		Console.WriteLine($"Total Words in file {_fileUri} : {split_text.Length}");
+	}
+	
+	public void CountLines()
+	{
+		int counter = 0;	
+		foreach(char c in _content)
+		{
+			if (c == '\n')
+			{
+                counter++;
+            }
+		}
+		Console.WriteLine($"Total Lines in file {_fileUri} : {counter}");
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+using System;
+using Dolphin;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		if (args.Length > 0)
+		{
+			ParseArg(args);
+		}
+		else
+		{
+			PrintHelp();
+		}
+		//Console.ReadKey();
+	}
+
+	static void PrintHelp()
+	{
+		string helpText = "" +
+		"Usage: Dolphin [arguments]\n\n" +
+		"arguments:\n" +
+		"   --letters <file>       count AlphaNumeric\n" +
+		"   --chars  <file>        count all characters\n" +
+		"   --words <file>         count Words\n" +
+		"   --lines <file>         count Lines\n" +
+		"   <file>                 calculate all above\n"+
+		"   --help                 print help\n";
+		
+		Console.WriteLine(helpText);
+	}
+	
+	static void ParseArg(string[] args)
+	{
+		string[] options = {"--letters", "--chars", "--words", "--lines"};
+	
+		string Arg1 = args[0];
+		// TODO fix this
+		string Arg2 = "";
+		if(args.Length > 1)
+		{
+			Arg2 = args[1];
+		}
+
+		if(Arg1 == "--help")
+		{
+			PrintHelp();
+			return;
+		}
+		
+		// Part 1 Where We only have File.
+		if(File.Exists(Arg1))
+		{
+			FileAction file = new FileAction(Arg1);
+			file.AllAction();
+			return;
+		}
+		// Part 2 where we have Arguments and File.
+		else if(options.Contains(Arg1) && File.Exists(Arg2))
+		{
+			FileAction file = new FileAction(Arg2);
+			switch(Arg1)
+			{
+			case "--letters":
+				file.CountAlphaN();
+				break;
+			case "--chars":
+				file.CountChars();
+				break;
+			case "--words":
+				file.CountWords();
+				break;
+			case "--lines":
+				file.CountLines();
+				break;
+			}
+		}		
+		else
+		{
+			Console.WriteLine("Invalid Arguments!!");
+			PrintHelp();
+		}
+	}
+}
